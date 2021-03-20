785d9d41a1a5e56c54dfc77360841c27e0d9b67b
Julia Bitschnau <julia.bitschnau@bluewin.ch>
2021-03-20 15:51:34 +0100

    Added humidity and wind data

--- src/app.js
+++ src/app.js
@@ -2,9 +2,13 @@ function displayTemperature(response) {
   let tempeartureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
   tempeartureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "2b0b6250896d4e6a0a3b6c088976e629";
